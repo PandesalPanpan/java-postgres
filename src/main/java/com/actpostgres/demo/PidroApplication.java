fcccde17a156ab579221121faf434f8cbba6ef91
PandesalPanpan <petermarticio@gmail.com>
2025-12-10 15:40:06 +0800

    Initial commit

---
+++ src/main/java/com/actpostgres/demo/PidroApplication.java
@@ -0,0 +1,13 @@
+package com.actpostgres.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actpostgres/demo/PidroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actpostgres.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
